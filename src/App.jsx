02d22501d2977d1fd2fdd7be2ecb46842383f3a3
yeon-bo <dylk97@gamil.com>
2022-07-21 07:29:59 +0900

    Feat clone`

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import styled from "styled-components";
+import { Header, GameBoard } from "./components";
+import { BEST_SCORE_KEY } from "./constants";
+import GlobalStyle from "./styles/GlobalStyle";
+
+function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [reset, setReset] = useState(false);
+  useEffect(() => {
+    if (bestScore < score) {
+      setBestScore(score);
+      localStorage.setItem(BEST_SCORE_KEY, score);
+    }
+  }, [score]);
+
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Header
+        score={score}
+        bestScore={bestScore}
+        reset={reset}
+        setReset={setReset}
+      />
+      <Main>
+        <GameBoard
+          score={score}
+          setScore={setScore}
+          setBestScore={setBestScore}
+          reset={reset}
+          setReset={setReset}
+        ></GameBoard>
+      </Main>
+    </div>
+  );
+}
+
+export default App;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2rem;
+`;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Header } from "./Header";
+export { default as GameBoard } from "./GameBoard";
+export { default as GameModal } from "./GameModal";
+export { default as GridContainer } from "./GridContainer";
+export { default as ScoreBoard } from "./ScoreBoard";
+export { default as TileContainer } from "./TileContainer";
